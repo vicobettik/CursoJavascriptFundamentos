0c827bfb190a607a14b5b899f2d49a00ad09d2cb
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-03-19 13:35:19 -0600

    primitivos

--- assets/js/primitivos.js
+++ assets/js/primitivos.js
@@ -21,3 +21,9 @@ console.log(superPoder)
 
 let soyNull = null;
 console.log(typeof soyNull)
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(typeof symbol1);
+console.log(symbol1 === symbol2);
