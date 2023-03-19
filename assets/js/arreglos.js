ac16f58f59b9f5e60a46f0f92b4271cd6a63dbd4
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-03-19 14:33:51 -0600

    arreglos

---
+++ assets/js/arreglos.js
@@ -0,0 +1,20 @@
+// const arr = new Array(10);
+// const arr1 = [];
+// console.log(arr);
+// console.log(arr1);
+
+let videoJuegos = ['mario3', 'Megaman', 'Chrono trigger'];
+console.log({ videoJuegos })
+console.log(videoJuegos[0])
+
+let arregloCosas = [
+    true,
+    123,
+    'Victor',
+    1 + 2,
+    function(){},
+    () => {},
+    ['mario3', 'Megaman', 'Chrono trigger']
+]
+console.log({arregloCosas})
+console.log(arregloCosas[6][0])
