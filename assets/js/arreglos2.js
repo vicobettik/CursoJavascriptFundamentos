399a5f8633c594ee66ec6b51f7d0f8e6dbd2620c
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-03-19 22:56:52 -0600

    arreglos clase 2

---
+++ assets/js/arreglos2.js
@@ -0,0 +1,27 @@
+let juegos = ['zelda', 'mario', 'metroid', 'elden ring'];
+console.log(`Largo: ${juegos.length}`);
+
+let primero = juegos[0];
+let ultimo = juegos[juegos.length - 1];
+
+console.log({ primero, ultimo });
+
+juegos.forEach((elemento, indice, arr) => {
+    console.log({ elemento, indice, arr });
+});
+
+let nuevaLongitud = juegos.push('F-Zero');
+console.log({ nuevaLongitud, juegos })
+
+nuevaLongitud = juegos.unshift('Fire emblem');
+console.log({ nuevaLongitud, juegos });
+
+let juegoBorrado = juegos.pop();
+console.log({ juegoBorrado, juegos })
+
+let pos = 1;
+let juegosBorrados = juegos.splice(pos, 2);
+console.log({ juegosBorrados, juegos });
+
+let metroidIndex = juegos.indexOf('metroid');
+console.log({ metroidIndex });
