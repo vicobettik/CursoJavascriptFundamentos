3109740150f6e1f471f36486a21dbe8a9cb209fd
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-03-20 13:40:56 -0600

    funciones de flecha y retorno

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,6 +1,7 @@
 function saludar(nombre) {
     console.log(arguments)
     console.log(`Hola ${nombre}`);
+    return [1,2,3,4,5];
 }
 
 const saludar2 = function (nombre) {
@@ -11,6 +12,14 @@ const saludarFlecha = (nombre) => {
     console.log(`Hola ${nombre}`);
 }
 
-saludar('victor', 12, 'cdmx', 32);
-saludar2('victor2');
-saludarFlecha('Frida')
+const sumar = (a,b) =>  a + b;
+
+const getAleatorio = () => Math.random();
+
+const retornoSaludar = saludar('victor', 12, 'cdmx', 32);
+console.log(retornoSaludar);
+
+console.log(sumar(3,5));
+console.log(getAleatorio());
+// saludar2('victor2');
+// saludarFlecha('Frida')
