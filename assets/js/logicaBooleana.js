7f3c9e834427ab7dcef7b7f1108ce399304e59c6
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2023-03-22 11:20:09 -0600

    asignacion logica booleana

--- assets/js/logicaBooleana.js
+++ assets/js/logicaBooleana.js
@@ -8,23 +8,40 @@ const regresaFalse = () => {
     return false;
 }
 
-console.warn('Not o negacion');
-console.log(true)
-console.log(!true)
-console.log(!false)
-console.log(!regresaTrue())
+// console.warn('Not o negacion');
+// console.log(true)
+// console.log(!true)
+// console.log(!false)
+// console.log(!regresaTrue())
 
-console.warn('And');
-console.log(true && true)
-console.log(true && false)
-console.log(false && true)
-console.log(false && false)
+// console.warn('And');
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
 
-console.log('============');
-console.log(regresaFalse() && regresaTrue());
+// console.log('============');
+// console.log(regresaFalse() && regresaTrue());
 
-console.warn('Or');
-console.log(true || true)
-console.log(true || false)
-console.log(false || true)
-console.log(false || false)
+// console.warn('Or');
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+console.warn('Asignaciones');
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = true && 'Hola mundo';
+const a2 = 'Hola' && 'Mundo' &&soyFalso
+const a3 = soyFalso || 'Ya no soy falso'
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo' || true;
+console.log({a1});
+console.log({a2});
+console.log({a3});
+console.log({a4});
+console.log({a5});
